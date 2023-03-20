c68cd84914b275b8d364f9b08af719bc3ddf743a
alessiaBer <alexdeyn@gmail.com>
2023-03-20 13:05:55 +0100

    [add] generate random integer numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,3 +134,42 @@ playBtn.addEventListener('click', function() {
     }
 
 })
+
+
+/* Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+nella stessa cella può essere posizionata al massimo una bomba, 
+perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella:
+se il numero è presente nella lista dei numeri generati
+abbiamo calpestato una bomba
+la cella si colora di rosso e la partita termina.
+Altrimenti
+la cella cliccata si colora di azzurro
+l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+
+BONUS:
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe; */
+
+//genero 16 numeri casuali e li assegno ad un array
+//i numeri devono essere tutti diversi
+
+const maxRandomNumbs = 16;
+
+const generatedNumb = [];
+
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+for (let i = 1; i <= maxRandomNumbs; i++) {
+    const randomNumb = getRandomInteger(1, 100);
+    generatedNumb.push(randomNumb);
+    console.log(generatedNumb);
+}
+
